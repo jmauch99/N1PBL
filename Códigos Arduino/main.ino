d76bfdfe42353e858a7c8cb47814cec0d2cc1834
luanfelixlima <luan.felix.lima@outlook.com>
2024-09-16 09:49:31 -0300

    Implementando RTC e EEPRON

--- Códigos Arduino/main.ino
+++ Códigos Arduino/main.ino
@@ -1,11 +1,17 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <DHT.h> // Carrega a biblioteca DHT
+#include <RTClib.h>
+#include <EEPROM.h>
 
 // Define a ligação ao pino de dados do sensor
 #define DHTPIN 4
 #define DHTTYPE DHT22
 #define LDR_PIN A0
+#define UTC_OFFSET -3
+
+// Cria objeto RTC
+RTC_DS1307 RTC;
 
 // Cria objeto dht
 DHT dht(DHTPIN, DHTTYPE);
@@ -18,54 +24,208 @@ int ValorLDR = 0; // Armazenar a leitura do sensor LDR
 int IntensidadeLuz; // Transforma a leitura em uma escala de 0 a 100
 int l;
 
+// Configurações da EEPROM
+const int maxRecords = 100;
+const int recordSize = 8; // Tamanho de cada registro em bytes
+int startAddress = 0;
+int endAddress = maxRecords * recordSize;
+int currentAddress = 0;
 
 // DEFININDO AS ENTRADAS DOS LEDS
 int redPin = 11;
 int yellowPin = 10;
 int greenPin = 9;
 
+// buzzer
+const int buzzer = 6;
 
 // Variáveis para criação de caracteres customizados
-byte LuzFraca[8] = { 
-  B00000, 
+byte Grau[8] = { 
+  B01110, 
   B10001, 
+  B01110, 
   B00000, 
+  B00000,
   B00000, 
-  B11111, 
-  B10001, 
-  B00000 
+  B00000,
+  B00000
 };
 
-byte LuzMedia[8] = { 
-  B00000, 
-  B10001, 
-  B00000, 
-  B00000, 
-  B11111, 
-  B00000, 
-  B00000 
+// Define os caracteres personalizados
+byte char1[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111
 };
 
-byte LuzForte[8] = { 
-  B00000, 
-  B10001, 
-  B00000, 
-  B10001, 
-  B11111, 
-  B00000, 
-  B00000 
+byte char2[8] = {
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B11111
 };
 
-// Intervalos de tempo para alternar entre as informações (em milissegundos)
-const unsigned long intervaloTempUmidade = 10000; // 10 segundos
-const unsigned long intervaloLuminosidade = 10000; // 10 segundos
+byte char3[8] = {
+  B00000,
+  B10000,
+  B01000,
+  B00100,
+  B11110,
+  B00000,
+  B00000,
+  B11111
+};
 
-// Variáveis para controle de tempo
-unsigned long tempoAnterior = 0;
-unsigned long tempoAtual = 0;
-int telaAtual = 0;
+byte char4[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111
+};
+
+byte char5[8] = {
+  B10000,
+  B10000,
+  B10000,
+  B01000,
+  B00100,
+  B00111,
+  B00000,
+  B00000
+};
+
+byte char6[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B00000,
+  B00000
+};
+
+byte char7[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B00000,
+  B00000
+};
+
+byte char8[8] = {
+  B00001,
+  B00001,
+  B00001,
+  B00010,
+  B00100,
+  B11100,
+  B00000,
+  B00000,
+};
+
+struct DateTimeStruct {
+  uint8_t day;
+  uint8_t month;
+  uint16_t year; // Pode ser uint8_t se quiser apenas os últimos dois dígitos
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
+};
 
 void setup() {
+  lcd.begin(16, 2);              
+  lcd.clear();
+  lcd.backlight();    // Liga luz do LCD
+
+  // Cria os caracteres personalizados
+  lcd.createChar(0, char1);
+  lcd.createChar(1, char2);
+  lcd.createChar(2, char3);
+  lcd.createChar(3, char4);
+  lcd.createChar(4, char5);
+  lcd.createChar(5, char6);
+  lcd.createChar(6, char7);
+  lcd.createChar(7, char8);
+
+  // Exibe o texto "EM(BARCADOS)" na primeira linha
+  lcd.setCursor(2, 0); // Centraliza o texto na primeira linha
+  lcd.print("EM(BARCADOS)");    
+  delay(2000); // Aguarda 3 segundos
+
+  // Limpa a linha do texto para mostrar apenas os desenhos
+  lcd.clear();
+
+  // Exibe os caracteres personalizados nas posições especificadas
+  lcd.setCursor(6, 0);  
+  lcd.write(byte(0));
+  delay(500);
+  
+  lcd.setCursor(7, 0);  
+  lcd.write(byte(1));
+  delay(500);
+
+  lcd.setCursor(8, 0);  
+  lcd.write(byte(2));
+  delay(500);
+
+  lcd.setCursor(9, 0);  
+  lcd.write(byte(3));
+  delay(500);
+
+  lcd.setCursor(6, 1);  
+  lcd.write(byte(4));
+  delay(500);
+
+  lcd.setCursor(7, 1);  
+  lcd.write(byte(5));
+  delay(500);
+  
+  lcd.setCursor(8, 1);  
+  lcd.write(byte(6));
+  delay(500);
+  
+  lcd.setCursor(9, 1);  
+  lcd.write(byte(7));
+  delay(500);
+
+  // Exibe "DATA" e "LOGGER" centralizados
+  lcd.clear();
+  lcd.setCursor(6, 0); // Centraliza "DATA" na primeira linha
+  lcd.print("DATA");
+  
+  lcd.setCursor(5, 1); // Centraliza "LOGGER" na segunda linha
+  lcd.print("LOGGER");
+  delay(2000); // Aguarda 3 segundos
+
+  // Apaga tudo e exibe "carregando..." centralizado
+  lcd.clear();
+  lcd.setCursor(1, 0); // Centraliza "carregando..." na primeira linha
+  lcd.print("Carregando...");
+  delay(2000); // Aguarda 3 segundos
+  
+  // Limpa o display após a mensagem de carregando
+  lcd.clear(); 
+
+  // buzzer
+  pinMode(buzzer,OUTPUT); 
+
   // LEDs
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
@@ -74,113 +234,184 @@ void setup() {
 
   Serial.begin(9600); // Inicializa a serial
   lcd.init();         // Inicializa LCD
-  lcd.backlight();    // Liga luz do LCD
 
   // Cria os caracteres customizados
-  lcd.createChar(0, LuzFraca);
-  lcd.createChar(1, LuzMedia);
-  lcd.createChar(2, LuzForte);
+  lcd.createChar(3, Grau);
 
   dht.begin();
   pinMode(LDR_PIN, INPUT); // Define o pino do LDR como entrada
-  tempoAnterior = millis(); // Inicializa o tempo
+
+  // hora atual
+  RTC.begin();    // Inicialização do Relógio em Tempo Real
+  RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+   
+}
+
+
+void getNextAddress() {
+    currentAddress += recordSize;
+    if (currentAddress >= endAddress) {
+        currentAddress = 0; // Volta para o começo se atingir o limite
+    }
 }
 
 void loop() {
-  /* debugando leds
-  digitalWrite(redPin, HIGH); 
-  digitalWrite(yellowPin, HIGH); 
-  digitalWrite(greenPin, HIGH);*/ 
-
-  tempoAtual = millis();
-
-  if (tempoAtual - tempoAnterior >= intervaloTempUmidade && telaAtual == 0) {
-    mostrarTemperaturaEUmidade();
-    telaAtual = 1;
-    tempoAnterior = tempoAtual;
-  } 
-  else if (tempoAtual - tempoAnterior >= intervaloLuminosidade && telaAtual == 1) {
-    mostrarLuminosidade();
-    telaAtual = 0;
-    tempoAnterior = tempoAtual;
-  }
-
-
-
-
+  mostrarGravarSensores();
+  
 }
 
-void mostrarTemperaturaEUmidade() {
+void mostrarGravarSensores() {
+  /*   DHT   */
+
   float h = dht.readHumidity(); // Lê o valor da umidade
   float t = dht.readTemperature(); // Lê o valor da temperatura
 
-  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(t, 1);
-  lcd.write((byte)0); // Mostra o símbolo do grau
+  lcd.print("T: ");
+  lcd.print(round(t), 1);
+  lcd.write(byte(3)); // Mostra o símbolo do grau
   lcd.print("C");
 
-  lcd.setCursor(0, 1);
-  lcd.print("Umid: ");
-  lcd.print(h, 1);
+  lcd.setCursor(9, 0);
+  lcd.print("U: ");
+  lcd.print(round(h), 1);
   lcd.print("%");
 
+  /*Serial.println("");
   Serial.print("Temperatura: ");
   Serial.print(t);
   Serial.println(" °C");
   Serial.print("Umidade: ");
   Serial.print(h);
-  Serial.println(" %");
+  Serial.println(" %");*/
+
+
+  /*  LDR   */
+  ValorLDR = analogRead(LDR_PIN); // Lê o valor do sensor LDR
+  IntensidadeLuz = map(ValorLDR, 0, 1023, 100, 0); // Converte o valor para uma escala de 0 a 100
+
+  lcd.setCursor(0, 1);
+  lcd.print("Lumi: ");
+  lcd.print(IntensidadeLuz);
+  lcd.print("% ");
+
+  /*Serial.print("Intensidade de Luz 0 - 1023 = ");
+  Serial.println(ValorLDR);
+  Serial.print("Intensidade de Luz 0 - 100% = ");
+  Serial.println(IntensidadeLuz);*/
+
+
+  /*  ALERTAS   */
 
   ValorLDR = analogRead(LDR_PIN); // Lê o valor do sensor LDR
   l = map(ValorLDR, 0, 1023, 100, 0); // Converte o valor para uma escala de 0 a 100
 
   // Condições boas
   if ((t >= 18 && t <= 22) && (h >= 36 && h <= 44) && (l >= 5 && l <= 25))
-  {
+  {     
         digitalWrite(redPin, LOW); 
         digitalWrite(yellowPin, LOW); 
         digitalWrite(greenPin, HIGH);
   } 
-  else if ((t >= 15 && t < 18 || t > 22 && t <= 25) && (h >= 30 && h < 36 || h > 44 && h <= 50) && (l >=0 && l < 5 || l > 25 && l <= 30)) { 
+  // Condições ruins
+  else if ((t < 15 || t > 25) || (h < 30 || h > 50) || (l < 0 || l > 30)) { 
+        digitalWrite(redPin, HIGH); 
+        digitalWrite(yellowPin, LOW);
+        digitalWrite(greenPin, LOW);
+
+        // buzzer alerta
+        tone(buzzer,5000); 
+        delay(150);
+        noTone(buzzer); 
+  } 
+  // Condições de alerta
+  else {
         digitalWrite(redPin, LOW); 
         digitalWrite(yellowPin, HIGH); 
         digitalWrite(greenPin, LOW);
-  } 
-  else {
-        digitalWrite(redPin, HIGH); 
-        digitalWrite(yellowPin, LOW); 
-        digitalWrite(greenPin, LOW);
   }
 
-}
 
-void mostrarLuminosidade() {
-  ValorLDR = analogRead(LDR_PIN); // Lê o valor do sensor LDR
-  IntensidadeLuz = map(ValorLDR, 0, 1023, 100, 0); // Converte o valor para uma escala de 0 a 100
+  /*   RTC   */
+  
+  // Obtém a data e hora atuais
+  DateTime now = RTC.now();
+  int offsetSeconds = UTC_OFFSET * 3600; // Convertendo horas para segundos, se necessário
+  now = now.unixtime() + offsetSeconds; // Adicionando o deslocamento ao tempo atual
+  DateTime adjustedTime = DateTime(now);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Luminosidade:");
-  lcd.setCursor(0, 1);
-  lcd.print(IntensidadeLuz);
-  lcd.print("% ");
+  // Preenche a estrutura com os valores de data e hora
+  DateTimeStruct currentTime;
+  currentTime.day = adjustedTime.day();
+  currentTime.month = adjustedTime.month();
+  currentTime.year = adjustedTime.year();
+  currentTime.hour = adjustedTime.hour();
+  currentTime.minute = adjustedTime.minute();
+  currentTime.second = adjustedTime.second();
 
-  // Atualiza o ícone no LCD baseado na intensidade
-  if (IntensidadeLuz < 33) {
-    lcd.write(byte(0)); // LuzFraca
-  } else if (IntensidadeLuz < 66) {
-    lcd.write(byte(1)); // LuzMedia
-  } else {
-    lcd.write(byte(2)); // LuzForte
+
+  /*   EEPROM   */
+  int secondAsInt = adjustedTime.second();
+  
+  if (secondAsInt % 5 == 0)  // Gravando os dados a cada 5s
+  {
+    delay(500);
+
+    // gravando na memória
+    EEPROM.write(currentAddress, t);
+    currentAddress += 1;
+    EEPROM.write(currentAddress, h);
+    currentAddress += 1;
+    EEPROM.write(currentAddress, IntensidadeLuz);
+    currentAddress += 1;
+    EEPROM.put(currentAddress, currentTime);
+    currentAddress += 1;
+    
+    
+    // regastando últimos registros da memória
+    int i = 0;
+
+    Serial.print("Dados: ");
+    
+    for (int i = currentAddress - 4; i < currentAddress; i++) {
+        DateTimeStruct storedTime;
+
+        if (i == currentAddress - 4) {
+            Serial.print(EEPROM.read(i)); // lendo dados de temp do eeprom
+            Serial.print("°C/t ");
+        }
+        else if (i == currentAddress - 3) {
+            Serial.print(EEPROM.read(i)); // lendo dados de umi do eeprom
+            Serial.print("%/u ");
+        }
+        else if (i == currentAddress - 2) {
+            Serial.print(EEPROM.read(i)); // lendo dados de lumi do eeprom
+            Serial.print("%/l ");
+        }
+        else if (i == currentAddress - 1) {
+              EEPROM.get(i, storedTime); // lendo dados de hora do eeprom
+              // Exibe os valores lidos
+              Serial.print(" | Data e Hora: ");
+              Serial.print(storedTime.day);
+              Serial.print("/");
+              Serial.print(storedTime.month);
+              Serial.print("/");
+              Serial.print(storedTime.year);
+              Serial.print(" ");
+              Serial.print(storedTime.hour);
+              Serial.print(":");
+              Serial.print(storedTime.minute);
+              Serial.print(":");
+              Serial.println(storedTime.second);
+        }
+
+    }
+    
+    getNextAddress();
+    Serial.println("");
   }
-
-  Serial.print("Intensidade de Luz 0 - 1023 = ");
-  Serial.println(ValorLDR);
-  Serial.print("Intensidade de Luz 0 - 100% = ");
-  Serial.println(IntensidadeLuz);
 }
+
 
 
 
